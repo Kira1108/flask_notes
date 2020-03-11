fc7f906ed8d8852163545225711ecc5d5e06388d
Kira1108 <327485253@163.com>
2020-03-11 14:57:38 +0800

    Add templates, template inheritence, if statement, for statement

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,29 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World"
+
+
+@app.route('/test')
+def test_user():
+    user = {'username':'Huan'}
+    posts = [
+        {
+            "author":{"username":"Huan"},
+            "body":"Hi this is my first post."
+        },
+        {
+            "author":{"username":"Duan"},
+            "body":"I love your first post."
+        },
+        {
+            "author":{"username":"Suan"},
+            "body":"So do I."
+        },
+
+    ]
+
+    return render_template('index.html',title = 'Home',user = user,posts = posts)
